d9c05c2b437af3b76bfe123dbf29d0d8a20af93d
Karvonen Antti J <kxantti@da5-cs-bk107-30.helsinki.fi>
2015-12-17 12:40:17 +0200

    Basic class stucture, player movement and console printing

--- NethackSokoban/src/main/java/dev/nethacksokoban/Main.java
+++ NethackSokoban/src/main/java/dev/nethacksokoban/Main.java
@@ -1,8 +1,13 @@
 package dev.nethacksokoban;
 
+import dev.nethacksokoban.Game.Game;
+import dev.nethacksokoban.Util.InputScanner;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
- 
+        Game game = new Game(new InputScanner(new Scanner(System.in)));
+        game.startGame();
     }
 }
