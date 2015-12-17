f563ad541e2d6a94f4229af2b793f75b5b417975
Karvonen Antti J <kxantti@da5-cs-bk107-30.helsinki.fi>
2015-12-17 13:45:46 +0200

    PlayerTest added

---
+++ NethackSokoban/src/test/java/dev/nethacksokoban/Game/PlayerTest.java
@@ -0,0 +1,54 @@
+package dev.nethacksokoban.Game;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    private Player player;
+
+    public PlayerTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        player = new Player(2, 3);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void playerConstructor() {
+        assertEquals(2, player.getX());
+        assertEquals(3, player.getY());
+        assertEquals(0, player.getMoves());
+
+    }
+
+    @Test
+    public void move() {
+        player.move(3, 3);
+        assertEquals(3, player.getX());
+        assertEquals(3, player.getY());
+        assertEquals(1, player.getMoves());
+        player.move(2, 4);
+        assertEquals(2, player.getX());
+        assertEquals(4, player.getY());
+        assertEquals(2, player.getMoves());
+
+    }
+}
