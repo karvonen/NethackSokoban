540201f6637edab468d75abdda37402c6a0b01cd
karvonen <antti.karvonen@helsinki.fi>
2016-01-06 01:17:20 +0200

    Location and player tests

---
+++ NethackSokoban/src/test/java/dev/nethacksokoban/Game/LocationTest.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dev.nethacksokoban.Game;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author AAA
+ */
+public class LocationTest {
+
+    public LocationTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void differentRowComparison() {
+        Location locationTest1 = new Location(1, 2);
+        Location locationTest2 = new Location(2, 2);
+        assertEquals(false, locationTest2.equals(locationTest1));
+    }
+    
+    @Test
+    public void differentColComparison() {
+        Location locationTest1 = new Location(2, 3);
+        Location locationTest2 = new Location(2, 2);
+        assertEquals(false, locationTest2.equals(locationTest1));
+    }
+    
+    @Test
+    public void differentComparison() {
+        Location locationTest1 = new Location(1, 2);
+        Location locationTest2 = new Location(2, 3);
+        assertEquals(false, locationTest2.equals(locationTest1));
+    }
+
+    @Test
+    public void locationsComparison() {
+        Location locationTest1 = new Location(2, 2);
+        Location locationTest2 = new Location(2, 2);
+        assertTrue(locationTest2.equals(locationTest1));
+
+    }
+}
--- NethackSokoban/src/test/java/dev/nethacksokoban/Game/PlayerTest.java
+++ NethackSokoban/src/test/java/dev/nethacksokoban/Game/PlayerTest.java
@@ -33,23 +33,20 @@ public class PlayerTest {
 
     @Test
     public void playerConstructor() {
-        assertEquals(2, player.getRow());
-        assertEquals(3, player.getCol());
+        Location expectedLocation = new Location(2, 3);
+        assertEquals(expectedLocation, player.getLocation());
         assertEquals(0, player.getMoves());
-
     }
 
     @Test
-    public void move2() {
+    public void playerMove() {
         Location move1 = new Location(3, 3);
         player.setPlayerLocation(move1);
-        assertEquals(3, player.getRow());
-        assertEquals(3, player.getCol());
+        assertEquals(move1, player.getLocation());
         assertEquals(1, player.getMoves());
         Location move2 = new Location(2, 4);
         player.setPlayerLocation(move2);
-        assertEquals(2, player.getRow());
-        assertEquals(4, player.getCol());
+        assertEquals(move2, player.getLocation());
         assertEquals(2, player.getMoves());
     }
 }
